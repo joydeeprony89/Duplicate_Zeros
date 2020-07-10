3ded92226d86bcd598bee2cffd5a20bdd5ab4bcd
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-10 12:02:29 +0200

    Add project files.

---
+++ Duplicate Zeros/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Buffers;
+using System.Collections;
+
+namespace Duplicate_Zeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            DuplicateZeros(arr);
+            foreach (int i in arr) Console.WriteLine(i);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void DuplicateZeros(int[] arr)
+        {
+            int length = arr.Length;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == 0)
+                {
+                    i = i + 1;
+                    if (i < length)
+                    {
+                        for(int j = length -1; j > i; j--)
+                        {
+                            arr[j] = arr[j - 1];
+                        }
+                        arr[i] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
